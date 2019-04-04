237a37268b0cf84e7e199244a2ddf065c0290e92
Jolley,Chris <Chris.Jolley@Cerner.com>
2019-04-04 12:12:34 -0500

    Update RankedGradeBook.cs
    completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -42,5 +42,28 @@ namespace GradeBook.GradeBooks
                 return 'F';
             }
         }
+
+        public override void CalculateStatistics()
+        {
+            if(Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overal grade.");
+                return;
+            }
+
+            base.CalculateStatistics();
+            
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStudentStatistics(name);
+        }
     }
 }
