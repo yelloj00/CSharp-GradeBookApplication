f8d60ce1ac43cc9156441c827a4e335788d09d4c
Jolley,Chris <Chris.Jolley@Cerner.com>
2019-04-17 14:31:10 -0500

    completed module

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -4,7 +4,7 @@ namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook : BaseGradeBook
     {
-            public StandardGradeBook(string name) : base(name)
+            public StandardGradeBook(string name, bool isWeigthed) : base(name, isWeigthed)
             {
                 Type = GradeBookType.Standard;
             }
